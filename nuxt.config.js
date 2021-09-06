e0f655f3aff818716207bdd41007e39430c74065
Nick Niles <nick@nickniles.com>
2021-09-06 13:13:06 +0200

    [feat] better page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'nn-nuxt21',
+        title: 'Nick Niles - Web Design & Development',
         htmlAttrs: {
             lang: 'en'
         },
